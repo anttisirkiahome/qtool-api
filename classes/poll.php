28848cfd1d33cd1cf1427932db2058c31b43611b
antti <sirkia.antti@gmail.com>
2014-06-20 19:24:40 +0300

    save poll

--- classes/poll.php
+++ classes/poll.php
@@ -11,9 +11,31 @@ class Poll {
 		if( !isset($poll) || !isset($poll['duration']) || !isset($poll['question']) || !isset($poll['theme']) || !isset($poll['answers']) ) {
 			return false;
 		}  else {
+			$theme = ORM::for_table('themes')->where('name', $poll['theme'])->find_one();
+			if(!isset($theme->ID)) {
+				return false;
+			}
+			//FIXME missing user_id
+			$newPoll = ORM::for_table('poll')->create();
+			$newPoll->duration = $poll['duration'];
+			$newPoll->theme = $theme->ID;
+			$newPoll->question = $poll['question'];
+			$newPoll->save();
+
+			$id = $newPoll->id();
+
+			$i = 1;
+			foreach ($poll['answers'] as &$answer) {
+				$answers = ORM::for_table('answer')->create();
+				$answers->answer = $answer;
+				$answers->order = $i;
+				$answers->poll_ID = $id;
+				$answers->save();	
+				$i++;
+			}
+			
 			return true;	
 		}
-		
-
 	}
+
 }
--- poller.php
+++ poller.php
@@ -3,6 +3,10 @@ header('Access-Control-Allow-Origin: *');
 header('Content-Type: text/event-stream');
 header('Cache-Control: no-cache');
 header('Connection: keep-alive');
+require 'classes/idiorm.php';
+require 'classes/poll.php';
+require 'config.php';
+
 while(true) {
 
 	//TODO refactor this to be the poll requester!
@@ -14,6 +18,7 @@ while(true) {
 	//if no new polls..
 	//$ret = array('success' => juu);
 	//$ret = array('s' => true);  
+	
 	$kissa = rand(0,20);         
 	echo "data: " . json_encode(array('j' => $kissa, 'newPoll' => true)) . " \n\n";
 	ob_flush();
